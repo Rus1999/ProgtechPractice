77aa14cf4eb036e0cd000b18b615a7fb3139a132
Rus Progtech <punyarit07@gmail.com>
2023-05-22 16:15:33 +0700

    born to dev javascrip open access course

---
+++ BornToDev/Javascript/basic.js
@@ -0,0 +1,37 @@
+console.log("Hello Punyarit Klaphachon")
+// alert("Hello Punyarit Klaphachon")
+
+var name = "Punyarit Klaphachon"
+console.log(name)
+name = "Rus"
+console.log(name)
+
+var firstName = "Punyarit"
+var lastName = "Klaphachon"
+console.log(firstName)
+console.log(lastName)
+// alert(firstName + "  " + lastName)
+
+console.log(typeof true)
+
+console.log(10 * 2)
+console.log(10 * 2 ** 5)
+console.log("Nickname: " + "Rus")
+name = "Connect string" + "to another string"
+console.log(name)
+var sum = (4+2*2/2*2+4+1-5+19)+91
+console.log(sum)
+
+{
+    var globalX = 1
+    var localX = 1
+}
+console.log(localX)
+var x = 20
+{
+    var x = 100
+}
+console.log(x)
+
+nameFromPrompt = prompt("Please enter your name")
+console.log("Your name is: " + nameFromPrompt)
