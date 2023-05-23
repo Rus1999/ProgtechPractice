efb69b2831d70a0dedd06bec5ebf692f36f5cd08
Rus <punyarit07@gmail.com>
2023-05-23 18:18:00 +0700

    23 may 2023

--- BornToDev/Javascript/agerestric.js
+++
@@ -1,5 +0,0 @@
-let age = prompt("Please enter your age")
-if (age > 13)
-{
-    document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/EnollQ4BZ7A\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
-}
